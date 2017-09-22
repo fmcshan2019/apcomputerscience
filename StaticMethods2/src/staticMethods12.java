311b3b4d0b488e3fee945d6c4affd57e6a1f9456
fmcshan2019 <fmcshan2019@cchsraider.net>
2017-09-22 14:23:35 -0400

    initial commit

---
+++ StaticMethods2/src/staticMethods12.java
@@ -0,0 +1,12 @@
+
+public class staticMethods12 {
+	
+	public static void helloWorld2() {
+	System.out.println("Hello \n \n \nWorld");
+	}
+	
+	public static void main(String[] args) {
+		helloWorld2();
+	}
+
+}
