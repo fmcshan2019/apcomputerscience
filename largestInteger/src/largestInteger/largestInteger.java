5de580a714f48f735eb248bdcb566831815aec5e
frmcshan <frmcshan@10.0.0.28>
2017-10-03 16:46:20 -0400

    added comments and finished calculations to largest int project

--- largestInteger/src/largestInteger/largestInteger.java
+++ largestInteger/src/largestInteger/largestInteger.java
@@ -1,28 +1,28 @@
-package largestInteger;
-
 //Frank McShan -- Displays largest of three integers
-import java.util.Scanner;
+package largestInteger;
+import java.util.Scanner; //imports scanner
 public class largestInteger { //class
-	public static int largestInt(int num1, int num2, int num3) { 
+
+	public static int largestInt(int num1, int num2, int num3) {  //statement creates three parameters and return value will be an integer
+		//three statements below create three variables as integers
 		int firstMax;
 		int secondMax;
 		int greatest;
-		
-		firstMax = Math.max(num1, num2);
-		secondMax = Math.max(num2, num3);
-		greatest = Math.max(firstMax, secondMax);
-		
-		return greatest;
+		firstMax = Math.max(num1, num2); //finds the max of num1 and num2
+		secondMax = Math.max(num2, num3); //finds the max of num2 and num3
+		greatest = Math.max(firstMax, secondMax); //finds the max of the two answers from the calculations above
+		return greatest; //returns the greatest of the three integers
 	}
 	public static void main(String[] args) {
-		Scanner int11 = new Scanner(System.in);  //creates scanner for first integer
-		Scanner int22 = new Scanner(System.in);  //creates scanner for second integer
-		Scanner int33 = new Scanner(System.in);  //creates scanner for third integer
-		
-		largestInt();
-		
-		System.out.println("The largest of the three integers is" + greatest);
-
+		int num11; //creates three variables as integers... these are the ones that the user inputs
+		int num22;
+		int num33;
+		System.out.println("Enter three integers to find the largest of them:");
+		Scanner scanner1 = new Scanner(System.in);  //creates scanner for integers
+		num11 = scanner1.nextInt(); //initiates scanner for all three variables
+		num22 = scanner1.nextInt();
+		num33 = scanner1.nextInt();
+		int ans = largestInt(num11, num22, num33); //uses method to find the largest integer of the three integers imported from the user
+		System.out.println("The largest of the three integers is " + ans); //displays answer
 	}
-
 }
